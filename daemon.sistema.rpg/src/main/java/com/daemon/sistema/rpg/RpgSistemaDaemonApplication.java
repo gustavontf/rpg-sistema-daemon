ba35c797a327cd43107f75ecba6d7f354eebeb4e
Gusavo Neres <ntfgustavo@gmail.com>
2022-10-01 23:35:37 -0300

    Iniciando projeto spring #1

---
+++ daemon.sistema.rpg/src/main/java/com/daemon/sistema/rpg/RpgSistemaDaemonApplication.java
@@ -0,0 +1,14 @@
+package com.daemon.sistema.rpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgSistemaDaemonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgSistemaDaemonApplication.class, args);
+	}
+
+
+}
---
+++ daemon.sistema.rpg/src/test/java/com/daemon/sistema/rpg/RpgSistemaDaemonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daemon.sistema.rpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgSistemaDaemonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
