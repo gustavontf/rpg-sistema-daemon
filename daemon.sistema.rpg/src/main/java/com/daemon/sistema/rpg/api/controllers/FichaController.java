9acfc54684c1a60334bd7b48b9b2757a0f93f1a6
Gusavo Neres <ntfgustavo@gmail.com>
2022-11-27 04:23:10 -0300

    Metodo POST da Entidade Ficha

---
+++ daemon.sistema.rpg/src/main/java/com/daemon/sistema/rpg/api/controllers/FichaController.java
@@ -0,0 +1,29 @@
+package com.daemon.sistema.rpg.api.controllers;
+
+import com.daemon.sistema.rpg.api.models.FichaModel;
+import com.daemon.sistema.rpg.api.services.FichaService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/ficha")
+public class FichaController {
+
+    final FichaService fichaService;
+
+    public FichaController(FichaService fichaService) {
+        this.fichaService = fichaService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> salvarFicha(@RequestBody FichaModel fichaModel) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(fichaService.salvar(fichaModel));
+    }
+
+    @GetMapping
+    public String index() {
+        return "Olá mundo!";
+    }
+
+}
---
+++ daemon.sistema.rpg/src/main/java/com/daemon/sistema/rpg/api/models/FichaModel.java
@@ -0,0 +1,38 @@
+package com.daemon.sistema.rpg.api.models;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "FICHA")
+public class FichaModel implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, length = 200)
+    private String nome;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
+
+
+
+
